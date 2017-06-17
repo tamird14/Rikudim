3e73904b1662cadef54c3e01bae8b5c150a0ea19
Tamir David <tamird14@gmail.com>
2017-06-18 02:02:46 +0300

    add angular material

--- app/js/controllers/FormController.js
+++ app/js/controllers/FormController.js
@@ -5,6 +5,37 @@ app.controller('FormController', ['$scope', function ($scope) {
     $scope.year = ""
     $scope.startYear = 1924;
     $scope.endYear = new Date().getFullYear();
+    $scope.songs = ["Dolly Parton - Working 9 To 5 10",
+        "Good Lovin - Salsa",
+        "King Of Africa - La Bomba",
+        "New York - Frank Sintra",
+        "אבא",
+        "אבא אמא",
+        "אבא שולם",
+        "אבא שמעון"];
+    $scope.creators = ["גדי ביטון",
+        "שלמה ממן",
+        "אלעד פרל",
+        "ירון בן שמחון"];
+
+    $scope.completeSong = function (query) {
+        var songsMatch = [];
+        for (var song in $scope.songs) {
+            if ($scope.songs[song].includes(query.trim())) {
+                songsMatch.push($scope.songs[song]);
+            }
+        }
+        return songsMatch;
+    };
+    $scope.completeCreator = function (query) {
+        var creatorssMatch = [];
+        for (var creator in $scope.creators) {
+            if ($scope.creators[creator].includes(query.trim())) {
+                creatorssMatch.push($scope.creators[creator]);
+            }
+        }
+        return creatorssMatch;
+    };
 }]);
 
 app.filter('rangeYear', function () {
--- app/js/controllers/MainController.js
+++ app/js/controllers/MainController.js
@@ -5,4 +5,22 @@ app.controller('MainController', ['$scope', 'search', function ($scope, search) 
             $scope.dataRecieved = result.data.songs;
         })
     };
-}]);
+}]).config(function ($mdThemingProvider) {
+    $mdThemingProvider.theme('dark-grey').backgroundPalette('grey');
+    $mdThemingProvider.theme('dark-orange').backgroundPalette('orange').dark();
+    $mdThemingProvider.theme('dark-purple').backgroundPalette('deep-purple').dark();
+    $mdThemingProvider.theme('dark-blue').backgroundPalette('blue');
+});
+app.directive('tableRow', function (attr) {
+    return {
+        template: '<tr"><td>{{'+attr.name+'}}</td></tr>'
+        /*'<td width="4%">' +
+        '<div class="squaredTwo"><input type="checkbox" value="None" id="r5" name="check"></div>' +
+        '</td>' +
+        '<td width="30%">'+attr.name+'</td>' +
+        '<td width="30%">'+attr.creator+'</td>' +
+        '<td width="18%">'+attr.type+'</td>' +
+        '<td width="18%">'+attr.year+'</td>' +
+        '<td style="display: none;">'+attr.id+'</td></tr>'*/
+    };
+});
